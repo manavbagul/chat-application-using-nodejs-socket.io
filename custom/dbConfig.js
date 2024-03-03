7fc63e5908e5f8cb78b65df2593de2e84befe2b0
Manav Bagul <manavbagul.profile@gmail.com>
2024-03-03 11:47:09 +0530

    Update dbConfig.js
    NOW CONNECTION STRING CAN BE ACCESSIBLE THROUGH ENV

--- custom/dbConfig.js
+++ custom/dbConfig.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 isProduction = process.env.NODE_ENV === "production";
-const conenctionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_ENDPOINT}/${process.env.PG_DB}`
 
 const pool = new Pool({
-    connectionString: conenctionString
+    connectionString: process.env.CONNECTION_STRING
 });
 //connectionString: isProduction ? process.env.DATABASE : conenctionString
 
